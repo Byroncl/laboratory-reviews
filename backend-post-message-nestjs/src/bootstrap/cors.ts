87d2c524f84582240ed114e65ec3e022cc3c981f
byroncl <re0byrongcl@gmail.com>
2026-06-15 21:12:52 -0500

    feat; enchanted coors

--- backend-post-message-nestjs/src/bootstrap/cors.ts
+++ backend-post-message-nestjs/src/bootstrap/cors.ts
@@ -8,8 +8,21 @@ import {
 export const setupCors = (app: INestApplication): void => {
   const isProd = process.env.NODE_ENV === NODE_ENV.PRODUCTION;
 
+  const allowedOrigins = isProd
+    ? [
+        'https://albatrosfrontend.quetsana.com',
+        'https://albatrosbackend.quetsana.com',
+      ]
+    : [
+        'http://localhost:3024',
+        'http://localhost:3025',
+        'http://localhost:4200',
+        'http://127.0.0.1:3024',
+        'http://127.0.0.1:3025',
+      ];
+
   app.enableCors({
-    origin: true,
+    origin: allowedOrigins,
     methods: [...CORS_METHODS],
     allowedHeaders: isProd ? [...CORS_ALLOWED_HEADERS_PROD] : '*',
     credentials: true,
